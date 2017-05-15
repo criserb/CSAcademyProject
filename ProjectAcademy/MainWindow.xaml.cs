416885475666cd70f3f0053e76fff67d27384c57
criserb <ka.badkowski@gmail.com>
2017-05-16 00:06:51 +0200

    small fixes

--- ProjectAcademy/MainWindow.xaml.cs
+++ ProjectAcademy/MainWindow.xaml.cs
@@ -31,12 +31,12 @@ namespace ProjectAcademy
             Close();
         }
         private void New_game_Click(object sender, RoutedEventArgs e)
-            {
+        {
             GenerateMazeViewGrid();
         }
 
         private void Ranking_Click(object sender, RoutedEventArgs e)
-        {
+        { 
             Window1 Window1 = new Window1();
             this.Close();
             Window1.Show();
@@ -44,12 +44,12 @@ namespace ProjectAcademy
 
         private void slider_Width_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            textBox_Width.Text = ((int)slider_Width.Value).ToString();
+            this.textBox_Width.Text = ((int)slider_Width.Value).ToString();
         }
 
         private void slider_Height_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            textBox_Height.Text = ((int)slider_Height.Value).ToString();
+            this.textBox_Height.Text = ((int)slider_Height.Value).ToString();
         }
 
         private void Btn_Generate_Maze_Click(object sender, RoutedEventArgs e)
@@ -66,33 +66,33 @@ namespace ProjectAcademy
 
         private void GenerateMazeViewGrid()
         {
-            Btn_Exit.Visibility = Visibility.Hidden;
-            Btn_New_game.Visibility = Visibility.Hidden;
-            Btn_Ranking.Visibility = Visibility.Hidden;
-            Btn_Generate_Maze.Visibility = Visibility.Visible;
-            Btn_Back.Visibility = Visibility.Visible;
-            slider_Height.Visibility = Visibility.Visible;
-            slider_Width.Visibility = Visibility.Visible;
-            textBox_Height.Visibility = Visibility.Visible;
-            textBox_Width.Visibility = Visibility.Visible;
-            lbl_Height.Visibility = Visibility.Visible;
-            lbl_Width.Visibility = Visibility.Visible;
-            groupBoxlabyrinthDiemnsion.Visibility = Visibility.Visible;
+            this.Btn_Exit.Visibility = Visibility.Hidden;
+            this.Btn_New_game.Visibility = Visibility.Hidden;
+            this.Btn_Ranking.Visibility = Visibility.Hidden;
+            this.Btn_Generate_Maze.Visibility = Visibility.Visible;
+            this.Btn_Back.Visibility = Visibility.Visible;
+            this.slider_Height.Visibility = Visibility.Visible;
+            this.slider_Width.Visibility = Visibility.Visible;
+            this.textBox_Height.Visibility = Visibility.Visible;
+            this.textBox_Width.Visibility = Visibility.Visible;
+            this.lbl_Height.Visibility = Visibility.Visible;
+            this.lbl_Width.Visibility = Visibility.Visible;
+            this.groupBoxlabyrinthDiemnsion.Visibility = Visibility.Visible;
         }
         private void MenuViewGrid()
         {
-            Btn_Exit.Visibility = Visibility.Visible;
-            Btn_New_game.Visibility = Visibility.Visible;
-            Btn_Ranking.Visibility = Visibility.Visible;
-            Btn_Generate_Maze.Visibility = Visibility.Hidden;
-            Btn_Back.Visibility = Visibility.Hidden;
-            slider_Height.Visibility = Visibility.Hidden;
-            slider_Width.Visibility = Visibility.Hidden;
-            textBox_Height.Visibility = Visibility.Hidden;
-            textBox_Width.Visibility = Visibility.Hidden;
-            lbl_Height.Visibility = Visibility.Hidden;
-            lbl_Width.Visibility = Visibility.Hidden;
-            groupBoxlabyrinthDiemnsion.Visibility = Visibility.Hidden;
+            this.Btn_Exit.Visibility = Visibility.Visible;
+            this.Btn_New_game.Visibility = Visibility.Visible;
+            this.Btn_Ranking.Visibility = Visibility.Visible;
+            this.Btn_Generate_Maze.Visibility = Visibility.Hidden;
+            this.Btn_Back.Visibility = Visibility.Hidden;
+            this.slider_Height.Visibility = Visibility.Hidden;
+            this.slider_Width.Visibility = Visibility.Hidden;
+            this.textBox_Height.Visibility = Visibility.Hidden;
+            this.textBox_Width.Visibility = Visibility.Hidden;
+            this.lbl_Height.Visibility = Visibility.Hidden;
+            this.lbl_Width.Visibility = Visibility.Hidden;
+            this.groupBoxlabyrinthDiemnsion.Visibility = Visibility.Hidden;
         }
     }
 }
